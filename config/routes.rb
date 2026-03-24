2eddd6fabdf6941c5c3250a2b5b20aa563efa191
mjennings754 <mitchellj@outlook.jp>
2026-03-24 12:34:28 +1100

    add dashboard + sessions"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "dashboard" => "dashboard#home"
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    get "logout" => :destroy
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
