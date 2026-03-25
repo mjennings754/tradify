6d12d48941d4d9d9955103e891db3e6cf4638cd0
mjennings754 <mitchellj@outlook.jp>
2026-03-25 13:49:19 +1100

    add to sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def login(user)
+        session[:user_id] = user.id
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def require_login
+        if session[:user_id]
+            :ok
+        else
+            redirect_to login_path
+        end
+    end
 end
